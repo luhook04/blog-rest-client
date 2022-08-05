89525d4c5e0d6973a861f349e8699f35db4c655f
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-05 15:11:28 -0400

    Add a fetch statement to fetch all the posts from the api
    Map over the returned posts and return the PostPreview component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PostPreview from "./PostPreview";
 
 const Home = () => {
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const req = await fetch(
+          "https://dry-hamlet-86450.herokuapp.com/api/posts"
+        );
+        if (req.status !== 200) {
+        }
+        const reqJson = await req.json();
+        setPosts(reqJson.posts);
+      } catch (err) {}
+    };
+    getPosts();
+  }, []);
+
   return (
-    <div>
-      <h1>Home Component</h1>
+    <div className="home-container">
+      {posts ? (
+        <div>
+          <div className="welcome-message">
+            <h3>Welcome to Captain Hook's Blog</h3>
+          </div>
+          <div className="post-previews">
+            {posts.map((post) => {
+              return (
+                <PostPreview key={post._id} post={post}></PostPreview>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <h2>No Posts</h2>
+      )}
       <PostPreview />
     </div>
   );
