9c338479707403a9b06df5fb2d42eb39e222a550
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-11 21:34:30 -0400

    Fix routing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="navbar-container">
-      <Link to="/">
+      <Link to="/blog-rest-client">
         <h2>Cap'n Hook's Blog</h2>
       </Link>
     </div>
